ef817bc6fcf7355ff923c576844fe66613091d50
taalaibek <sagynalievv.t@gmail.com>
2025-05-15 10:25:50 +0600

    front# add navigation

--- next-detail/src/config/constants.ts
+++ next-detail/src/config/constants.ts
@@ -5,15 +5,18 @@ export const ROUTES = {
 	PROFILE: '/profile',
 	LOGIN: '/auth/login',
 	REGISTER: '/auth/register',
+	EMAIL_CONFIRMATION: 'auth/email-confirmation',
 };
 
-export const ROUTE_URL = [
+export const NAVBAR_URL = [
 	{ name: 'Домой', path: '/' },
-	{ name: 'Профиль', path: '/profile' },
+	{ name: 'О нас', path: '/about' },
 	{ name: 'Услуги', path: '/services' },
+	{ name: 'Контакты', path: '/contacts' },
 ];
 
 export const API_ROUTES = {
 	LOGIN: '/auth/login',
 	REGISTER: '/auth/register',
+	EMAIL_CONFIRMATION: 'auth/email-confirmation',
 };
--- next-detail/src/services/verification.service.ts
+++ next-detail/src/services/verification.service.ts
@@ -1,10 +1,11 @@
 import { IUser } from '@/types/user';
 
 import axiosApi from '@/config/axioxApi';
+import { API_ROUTES } from '@/config/constants';
 
 class VerificationService {
 	public async newVerification(token: string | null) {
-		const response = await axiosApi.post<IUser>('auth/email-confirmation', {
+		const response = await axiosApi.post<IUser>(API_ROUTES.EMAIL_CONFIRMATION, {
 			token,
 		});
 		console.log('Verification service', response);
